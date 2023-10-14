55f6b26bc99fd45b4edd3e5800e4c6af32bd3a64
patryk.neubauer <patryk.neubauer@gmail.com>
2023-10-14 12:38:05 +0200

    Add tokenizer code.

---
+++ model_repository/tokenizer/1/model.py
@@ -0,0 +1,38 @@
+import os
+from typing import Dict, List
+
+import numpy as np
+import triton_python_backend_utils as pb_utils
+from transformers import AutoTokenizer, PreTrainedTokenizer, TensorType
+
+
+class TritonPythonModel:
+    tokenizer: PreTrainedTokenizer
+
+    def initialize(self, args: Dict[str, str]) -> None:
+        # https://github.com/triton-inference-server/python_backend/blob/main/src/python.cc
+        path: str = os.path.join(args["model_repository"], args["model_version"])
+        self.tokenizer = AutoTokenizer.from_pretrained(path)
+
+    def execute(self, requests) -> List[List[pb_utils.Tensor]]:
+        responses = []
+        for request in requests:
+            query = [
+                t.decode("UTF-8")
+                for t in pb_utils.get_input_tensor_by_name(request, "TEXT")
+                .as_numpy()
+                .tolist()
+            ]
+            tokens: Dict[str, np.ndarray] = self.tokenizer(
+                text=query, return_tensors=TensorType.NUMPY
+            )
+            # tensorrt uses int32 as input type, ort uses int64
+            tokens = {k: v.astype(np.int64) for k, v in tokens.items()}
+
+            outputs = list()
+            for input_name in self.tokenizer.model_input_names:
+                tensor_input = pb_utils.Tensor(input_name, tokens[input_name])
+                outputs.append(tensor_input)
+            inference_response = pb_utils.InferenceResponse(output_tensors=outputs)
+            responses.append(inference_response)
+        return responses
